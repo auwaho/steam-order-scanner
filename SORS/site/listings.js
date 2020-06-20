dbc4a9c7cbca8fa00beb97d1de04879b4c401f41
Artem Grigorov <47985643+wadahawk@users.noreply.github.com>
2020-06-20 15:54:14 +0300

    add license text

--- SORS/site/listings.js
+++ SORS/site/listings.js
@@ -11,6 +11,27 @@ String.prototype.extract = function (t, r) {
 
 const source = document.documentElement.outerHTML;
 
+/*
+  Steam Order Scanner - a browser extension for Steam Community Market 
+  that helps keep your orders profitable.
+  Copyright (C) 2020 Artem Hryhorov
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see {http://www.gnu.org/licenses/}.
+
+  Home: https://github.com/auwaho/steam-order-scanner
+*/
+
 // get last 24 hours sales
 var salesPerDay = 0;
 const salesGraph = JSON.parse(source.extract('var line1=', ';'));
